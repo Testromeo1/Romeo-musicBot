37b9e572610ca85b74c59c38c33820db3a6b400b
Testromeo1 <134147817+Testromeo1@users.noreply.github.com>
2024-01-09 17:56:24 +0530

    Update git.py

--- Romeo/core/git.py
+++ Romeo/core/git.py
@@ -71,5 +71,5 @@ def git():
             nrs.pull(config.UPSTREAM_BRANCH)
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
-        install_req("pip3 install -r Installer")
+        install_req("pip3 install -r requirements.txt")
         LOGGER(__name__).info(f"Fetching updates from Repo ...")
