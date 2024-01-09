bb54ecdc56a11fccc254180ec7beccb22ccd3d02
Romeo RJ <124047324+RRomeo-RJ@users.noreply.github.com>
2024-01-09 17:28:55 +0530

    😎

--- Romeo/plugins/play/play.py
+++ Romeo/plugins/play/play.py
@@ -18,7 +18,7 @@ from Romeo.utils.channelplay import get_channeplayCB
 from Romeo.utils.database import is_video_allowed
 from Romeo.utils.decorators.language import languageCB
 from Romeo.utils.decorators.play import PlayWrapper
-from Romeo.utils.bgtmusic.bk import command
+from Romeo.utils.romeomusic.rj import command
 from Romeo.utils.formatters import formats
 from Romeo.utils.inline.play import (livestream_markup,
                                           playlist_markup,
@@ -226,7 +226,7 @@ async def play_commnd(
                 and not config.SPOTIFY_CLIENT_SECRET
             ):
                 return await mystic.edit_text(
-                    "𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐂𝐚𝐧'𝐭 𝐏𝐥𝐚𝐲 𝐒𝐩𝐨𝐭𝐢𝐟𝐲 𝐓𝐫𝐜𝐤𝐬 𝐨𝐫 𝐏𝐥𝐚𝐲𝐥𝐢𝐬𝐭𝐬 𝐑𝐞𝐩𝐨𝐫𝐭 [𝐁𝐠𝐭 𝐂𝐡𝐚𝐭](https://t.me/Bgt_Chat)."
+                    "𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐂𝐚𝐧'𝐭 𝐏𝐥𝐚𝐲 𝐒𝐩𝐨𝐭𝐢𝐟𝐲 𝐓𝐫𝐜𝐤𝐬 𝐨𝐫 𝐏𝐥𝐚𝐲𝐥𝐢𝐬𝐭𝐬 𝐑𝐞𝐩𝐨𝐫𝐭 [ROMEO SUPPORT](https://t.me/RomeoBot_op)."
                 )
             if "track" in url:
                 try:
@@ -349,7 +349,7 @@ async def play_commnd(
                 await Bikashh.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(
-                    "There's An Error In The Bot Then Report [𝐁𝐠𝐭 𝐂𝐡𝐚𝐭](https://t.me/Bgt_chat) AN Error"
+                    "There's An Error In The Bot Then Report [ROMEO SUPPORT](https://t.me/RomeoBot_op) AN Error"
                 )
                 return await app.send_message(
                     config.LOG_GROUP_ID,
